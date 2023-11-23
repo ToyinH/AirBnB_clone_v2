889334b67955d3ba7e2ce072d272dceefa9e7511
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-23 04:35:52 +0000

    a script that prepares a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Connect to the MySQL server as root (adjust the credentials if needed)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
