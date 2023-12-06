217dd0837fbfc731735314eb55b7d1594e8b9857
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-12-06 18:53:45 +0000

    Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Preparing my webservers with this script
+
+
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo mkdir -p /data/web_static/shared/
+
+echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+
+sym_link="/data/web_static/current"
+
+target="/data/web_static/releases/test/"
+
+if [ -L "$sym_link" ]; then
+		sudo rm -rf "$sym_link"
+fi
+
+sudo ln -s "$target" "$sym_link"
+
+sudo chown -R $USER:$USER /data/
+
+replace_str="server_name _;\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}"
+
+sudo sed -i "s|server_name _;|$replace_str|" /etc/nginx/sites-enabled/default
+
+
+sudo service nginx restart
