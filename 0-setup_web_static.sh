b1959acc1baeb62cc4652fa1bae81166f7e49ba2
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-12-07 00:58:23 +0000

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,16 +6,17 @@ sudo apt-get install -y nginx
 
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir /data/
+sudo mkdir -p /data/
 
-sudo mkdir /data/web_static/
+sudo mkdir -p /data/web_static/
 
-sudo mkdir /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/
 
-sudo mkdir /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 
-sudo mkdir /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/
 
+sudo touch /data/web_static/releases/test/index.html
 
 # sudo mkdir -p /data/web_static/releases/test/
 
@@ -31,21 +32,13 @@ echo "<html>
 
 # echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
 
-sym_link="/data/web_static/current"
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-target="/data/web_static/releases/test/"
-
-#if [ -L "$sym_link" ]; then
-#		sudo rm -rf "$sym_link"
-#fi
-
-sudo ln -s -f "$target" "$sym_link"
-
-sudo chown -R "$USER":"$USER" /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 replace_str="server_name _;\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}"
 
-sudo sed -i "s|server_name _;|$replace_str|" /etc/nginx/sites-enabled/default
+sudo sed -i "s/server_name _;/$replace_str/" /etc/nginx/sites-enabled/default
 
 
 sudo service nginx restart
