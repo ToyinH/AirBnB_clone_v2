76dfb6e8565f98d3bb1499a2ca421d4099acd700
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-12-25 09:27:06 +0000

    updated with documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,5 +15,20 @@ def Hello_HBNB():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    """
+    Run the application when the script is executed directly.
 
+    This block of code is executed only if the Python script is run
+    directly, not imported as a module. It starts the Flask application,
+    allowing it to be accessed externally.
+
+    Usage:
+        0-hello_route.py
+
+    Args:
+        host (str): The IP address to bind the Flask application to.
+                    Defaults to "0.0.0.0" to allow external access.
+        port (int): The port number to run the Flask application on.
+                    Defaults to 5000.
+    """
+    app.run(host="0.0.0.0", port=5000)
