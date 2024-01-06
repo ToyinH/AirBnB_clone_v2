ef23288770c3decafa56f0d41dc01cb46b8345b3
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-06 18:50:46 +0000

    update

--- console.py
+++ console.py
@@ -1,4 +1,6 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
+
+
 # """ Console Module """
 # import cmd
 # import sys
@@ -338,8 +340,21 @@
 #     HBNBCommand().cmdloop()
 
 
-#!/usr/bin/python3
+
 """Defines the HBNB console."""
+
+
+# import cmd
+# import sys
+# from models.base_model import BaseModel
+# from models.__init__ import storage
+# from models.user import User
+# from models.place import Place
+# from models.state import State
+# from models.city import City
+# from models.amenity import Amenity
+# from models.review import Review
+
 import cmd
 from shlex import split
 from models import storage
